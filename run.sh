e1bd61cc510ad39fe0c6b50e5a0178bef9c90626
Khoa Duong <dnanhkhoa@live.com>
2019-04-15 02:25:34 +0900

    Fix batch script

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@
 #$ -m ea
 #$ -M dnakhoa.aist@gmail.com
 
-source ~/ds/bin/activate
-
 source /etc/profile.d/modules.sh
 module load python/3.6/3.6.5
 module load cuda/10.0/10.0.130
 module load cudnn/7.5/7.5.0
 module load nccl/2.4/2.4.2-1
+
+source ~/ds/bin/activate
 
 python main.py train
