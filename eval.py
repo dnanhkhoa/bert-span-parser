d545cbed493f7e4ee0b938eda6a64f82029af6a4
Khoa Duong <dnanhkhoa@live.com>
2019-06-15 23:24:27 +0900

    Update Eval

--- eval.py
+++ eval.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 # This code is adapted from https://github.com/junekihong/beam-span-parser
-
-import math
 from collections import defaultdict
 
 
@@ -10,30 +8,30 @@ from collections import defaultdict
 # Which was a problem for our Discourse experiments.
 # We have changed this to use our own faithful implementation of EVALB.
 def evalb(gold_trees, predicted_trees):
-    result = FScore()
     assert len(gold_trees) == len(predicted_trees)
+    score = FScore()
     for gold_tree, predicted_tree in zip(gold_trees, predicted_trees):
-        gold_phrase_tree = PhraseTree.parse(str(gold_tree.linearize()))
-        predicted_phrase_tree = PhraseTree.parse(str(predicted_tree.linearize()))
-        result += predicted_phrase_tree.compare(gold_phrase_tree)
-    return result
+        gold_phrase_tree = PhraseTree.parse(gold_tree.linearize())
+        predicted_phrase_tree = PhraseTree.parse(predicted_tree.linearize())
+        score += predicted_phrase_tree.compare(gold_phrase_tree)
+    return score
 
 
 class FScore(object):
-    def __init__(self, correct=0, predcount=0, goldcount=0):
+    def __init__(self, correct=0, pred_count=0, gold_count=0):
         self.correct = correct  # correct brackets
-        self.predcount = predcount  # total predicted brackets
-        self.goldcount = goldcount  # total gold brackets
+        self.pred_count = pred_count  # total predicted brackets
+        self.gold_count = gold_count  # total gold brackets
 
     def precision(self):
-        if self.predcount > 0:
-            return (100.0 * self.correct) / self.predcount
+        if self.pred_count > 0:
+            return (100.0 * self.correct) / self.pred_count
         else:
             return 0.0
 
     def recall(self):
-        if self.goldcount > 0:
-            return (100.0 * self.correct) / self.goldcount
+        if self.gold_count > 0:
+            return (100.0 * self.correct) / self.gold_count
         else:
             return 0.0
 
@@ -52,8 +50,8 @@ class FScore(object):
 
     def __iadd__(self, other):
         self.correct += other.correct
-        self.predcount += other.predcount
-        self.goldcount += other.goldcount
+        self.pred_count += other.pred_count
+        self.gold_count += other.gold_count
         return self
 
 
@@ -66,104 +64,70 @@ class PhraseTree(object):
         self.children = children  # list of PhraseTree objects
         self.sentence = sentence
         self.leaf = leaf  # word at bottom level else None
-
         self._str = None
-
-    def __str__(self):
-        if True or self._str is None:
-            children = [
-                child
-                for child in self.children
-                if (len(child.children) > 0 or child.leaf is not None)
-            ]
-
-            if len(children) != 0:
-                childstr = " ".join(str(c) for c in children)
-                self._str = "({} {})".format(self.symbol, childstr)
-            elif self.leaf is not None:
-                self._str = "({} {})".format(
-                    # self.sentence[self.leaf][1],
-                    self.symbol,
-                    self.sentence[self.leaf][0],
-                )
-            else:
-                self._str = ""
-        return self._str
 
     @staticmethod
     def parse(line):
-        """
-        Loads a tree from a tree in PTB parenthetical format.
-        """
-        line += " "
-        sentence = []
-        _, t = PhraseTree._parse(line, 0, sentence)
-
-        # if t.symbol == 'TOP' and len(t.children) == 1:
-        #    t = t.children[0]
-
-        return t
+        _, tree = PhraseTree._parse(line + " ", 0, [])
+        return tree
 
     @staticmethod
     def _parse(line, index, sentence):
-        "((...) (...) w/t (...)). returns pos and tree, and carries sent out."
-
         assert line[index] == "(", "Invalid tree string {} at {}".format(line, index)
+
         index += 1
         symbol = None
         children = []
         leaf = None
+
         while line[index] != ")":
             if line[index] == "(":
-                index, t = PhraseTree._parse(line, index, sentence)
-                children.append(t)
+                index, tree = PhraseTree._parse(line, index, sentence)
+                children.append(tree)
             else:
                 if symbol is None:
-                    # symbol is here!
                     rpos = min(line.find(" ", index), line.find(")", index))
-                    # see above N.B. (find could return -1)
-
-                    symbol = line[index:rpos]  # (word, tag) string pair
-
-                    index = rpos
+                    symbol = line[index:rpos]
                 else:
                     rpos = line.find(")", index)
                     word = line[index:rpos]
                     sentence.append((word, symbol))
                     leaf = len(sentence) - 1
-                    index = rpos
+
+                index = rpos
 
             if line[index] == " ":
                 index += 1
 
-        assert line[index] == ")", "Invalid tree string %s at %d" % (line, index)
+        assert line[index] == ")", "Invalid tree string {} at {}".format(line, index)
 
-        t = PhraseTree(symbol=symbol, children=children, sentence=sentence, leaf=leaf)
+        tree = PhraseTree(
+            symbol=symbol, children=children, sentence=sentence, leaf=leaf
+        )
 
-        return (index + 1), t
+        return (index + 1), tree
 
     def left_span(self):
         try:
             return self._left_span
         except AttributeError:
-            if self.leaf is not None:
-                self._left_span = self.leaf
-            else:
+            if self.leaf is None:
                 self._left_span = self.children[0].left_span()
+            else:
+                self._left_span = self.leaf
             return self._left_span
 
     def right_span(self):
         try:
             return self._right_span
         except AttributeError:
-            if self.leaf is not None:
-                self._right_span = self.leaf
-            else:
+            if self.leaf is None:
                 self._right_span = self.children[-1].right_span()
+            else:
+                self._right_span = self.leaf
             return self._right_span
 
     def brackets(self, advp_prt=True, counts=None):
-
         if counts is None:
             counts = defaultdict(int)
 
@@ -192,18 +156,15 @@ class PhraseTree(object):
         return counts
 
     def compare(self, gold, advp_prt=True):
-        """
-        returns (Precision, Recall, F-measure)
-        """
-        predbracks = self.brackets(advp_prt)
-        goldbracks = gold.brackets(advp_prt)
+        pred_bracks = self.brackets(advp_prt)
+        gold_bracks = gold.brackets(advp_prt)
 
         correct = 0
-        for gb in goldbracks:
-            if gb in predbracks:
-                correct += min(goldbracks[gb], predbracks[gb])
+        for gb in gold_bracks:
+            if gb in pred_bracks:
+                correct += min(gold_bracks[gb], pred_bracks[gb])
 
-        pred_total = sum(predbracks.values())
-        gold_total = sum(goldbracks.values())
+        pred_total = sum(pred_bracks.values())
+        gold_total = sum(gold_bracks.values())
 
         return FScore(correct, pred_total, gold_total)
